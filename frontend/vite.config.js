cff4edc823755b846bceb2c7bc275b7ce2ecb744
rahul singh <rahul2004ruby@gmail.com>
2025-03-17 01:11:32 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
-    },
-  },
+  plugins: [react()]
 });
